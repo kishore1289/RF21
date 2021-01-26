4873c146717c6739596dc3141a23d9a642ad59ac
Sony <Sony@192.168.1.117>
2021-01-26 14:56:38 +0530

    uploading java project into github

---
+++ github/src/github/openchromebrowser.java
@@ -0,0 +1,15 @@
+package github;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class openchromebrowser {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	public static void main(String[] args)
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.co.in");
+	}
+}
